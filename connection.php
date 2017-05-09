7c58718451fc3b9891ed2d4fa581504495d060ec
nishikawamakio <nishikawa.makiogizumo@gmail.com>
2017-05-09 13:12:59 +0900

    lesson12

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+  require_once('config.php');
+
+  function connectPdo() {
+    try{
+      return new PDO(DSN,DB_USER,DB_PASSWORD);
+    } catch (PDOException $e) {
+      echo $e->getMessage();
+    }
+  }
+?>
